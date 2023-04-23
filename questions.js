e195b47344c46a5aaa200de4898241410667cc5c
kmfox1087 <kmfox1087@gmail.com>
2023-04-23 12:26:27 -0400

    added js variables

---
+++ questions.js
@@ -0,0 +1,6 @@
+// Variables
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
+
