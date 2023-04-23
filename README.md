61ae029b7ccbab5b261b6431256f77271cfeffef
kmfox1087 <kmfox1087@gmail.com>
2023-04-23 13:50:28 -0400

    Added js variables and questions

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Test-3
+# _JavaScript Quiz_
+
+#### By _*Kylie Fox*_ 
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _JavaScript_
+
+## Description
+
+For Bootcamps 4th challenge, we had to make a JavaScript quiz. This allowed us to use the JavaScript we've been learning 
+and put our skills to the test to make a fun game with a timer and highscore board! 
+
+## User Story
+
+* AS A coding boot camp student
+* I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+* SO THAT I can gauge my progress compared to my peers
+
+## Acceptance Criteria
+
+* GIVEN I am taking a code quiz
+* WHEN I click the start button
+* THEN a timer starts and I am presented with a question
+* WHEN I answer a question
+* THEN I am presented with another question
+* WHEN I answer a question incorrectly
+* THEN time is subtracted from the clock
+* WHEN all questions are answered or the timer reaches 0
+* THEN the game is over
+* WHEN the game is over
+* THEN I can save my initials and my score
+
+## Setup/Installation Requirements
+
+* Go to [insert link to github repo here] and copy SSH 
+* Use _git clone_ in your terminal and copy and paste the SSH code to your local computer
+* Use _git status_ to make sure you are in the repo, if not use _cd Bootcamp-Challenge-3_ in your terminal to change into your local repo
+* After clone is complete, type _code ._ into your terminal to edit files as needed
+
+## Contributions
+
+* 
+
+## License
+
+Copyright (c) _4/10/23_ _Kylie Fox_
--- questions.js
+++ questions.js
@@ -1,6 +1,60 @@
 // Variables
+var score = 0;
+var questionIndex = 0;
+
 var currentTime = document.querySelector("#currentTime");
 var timer = document.querySelector("#startTime");
 var questionsDiv = document.querySelector("#questionsDiv");
 var wrapper = document.querySelector("#wrapper");
 
+var secondsLeft = 76;
+var holdInterval = 0;
+var penalty = 10;
+var ulCreate = document.createElement("ul");
+
+// Questions
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+
+];
+
+timer.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
+});
+ 
